018aa2f17ca641155c757bb25e3e7b9f3632791d
unknown <jandyroachy@gmail.com>
2025-02-08 12:08:34 +0530

    Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';  // Import directly
+import LoginPage from './pages/LoginPage';  
+import SignUpPage from './pages/SignUpPage';
+import Home from './pages/Home';
+import CreateProduct from "./pages/createProduct"
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/createProduct" element={<CreateProduct />} />
       </Routes>
     </BrowserRouter>
   );
