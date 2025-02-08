bd619abe077ee71cfb39718e8c923b56abf7397e
unknown <jandyroachy@gmail.com>
2025-02-08 12:13:53 +0530

    done

--- Backend/app.js
+++ Backend/app.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const app = express();
-
-const ErrorHandler = require('./utlis/errorHandler'); // Use the correct relative path
-
-
+const ErrorHandler = require('./utlis/errorHandler'); 
 const cookieParser = require("cookie-parser");
-
 const bodyParser = require("body-parser");
+
+const cors = require("cors")
+app.use(cors())
 app.use(express.json());
 
 app.use(cookieParser());
 app.use("/", express.static("uploads"));
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-
-// config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "./config/.env" });
 }
 
 const user = require("./controller/user");
+const productRoutes = require("./controller/product")
 app.use("/api/v2/user", user);
+app.use("/api/products", productRoutes);
 
-// Middleware for Errors
 app.use(ErrorHandler);
 
 module.exports = app;
